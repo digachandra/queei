9dc0416e9c2669a45d8be882d768d50e8d946de5
digachandra <imdigachandra@gmail.com>
2016-10-13 13:07:36 +0700

    Update API routes for users

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const users = require('../controllers/questions')
 const questions = require('../controllers/questions')
 const answers = require('../controllers/answers')
+
+router.post('/users', users.create)
+router.get('/users', users.read)
+router.put('/users/:id', users.update)
+router.delete('/users/:id', users.delete)
 
 router.post('/questions', questions.create)
 router.get('/questions', questions.read)
